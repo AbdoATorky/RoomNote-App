41ca81bccaccf088d1150db5f1b3cd5ffd92892c
AbdoATorky <abdalsalamtorky@gmail.com>
2024-09-29 02:09:40 +0300

    final commit

---
+++ app/src/main/java/com/abs/room/data/Note.kt
@@ -0,0 +1,18 @@
+package com.abs.room.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Note(
+    val title: String,
+    val description: String,
+    val dateAdded: Long,
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+
+
+
+
+)
